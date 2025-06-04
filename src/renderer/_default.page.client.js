3adf8b7546006b2a5d852ce2d4643d3b5be6a1a0
Ivan <83720387+Dufi1928@users.noreply.github.com>
2025-06-04 18:46:05 +0200

    Protect dashboard route

--- src/renderer/_default.page.client.js
+++ src/renderer/_default.page.client.js
@@ -1,16 +1,24 @@
 // src/renderer/_default.page.client.js
 import { createSSRApp, h } from 'vue'
 import { createPinia } from 'pinia'
+import { useAuthStore } from '../../stores/auth.js'
 
 export async function render(pageContext) {
-    // pageProps est désormais transmis automatiquement
     const { Page, pageProps = {} } = pageContext
 
     const app = createSSRApp({
         render: () => h(Page, pageProps)
     })
 
-    app.use(createPinia())
+    const pinia = createPinia()
+    app.use(pinia)
+
+    const auth = useAuthStore()
+
+    if (pageContext.urlPathname.startsWith('/protected') && !auth.isAuthenticated) {
+        window.location.href = '/login'
+        return
+    }
 
     app.mount('#app')
 }
