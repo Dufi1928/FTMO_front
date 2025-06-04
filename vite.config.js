282d59e4e990682946c53fd3b3c029c151102d5c
Ivan <83720387+Dufi1928@users.noreply.github.com>
2025-06-04 17:50:24 +0200

    Remove SSR plugin and legacy server files

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,11 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
-import ssr from 'vite-plugin-ssr/plugin'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    ssr(),
     vueDevTools(),
   ],
   resolve: {
